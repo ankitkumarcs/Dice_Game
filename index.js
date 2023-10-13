92daffee907562c952b127d9418e8395b2b7bbff
Ankit <ankittamori@gamil.com>
2023-10-13 22:20:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber + ".png";
+
+var randomImageSource = "./images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+//2 image
+
+var randomNumber2 = Math.floor(Math.random() *6) +1;
+var randomImageSource2 = "./images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+// Heading
+if(randomNumber > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins";   
+}else if(randomNumber < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins";
+}else{
+    document.querySelector("h1").innerHTML = "Draw";
+}
